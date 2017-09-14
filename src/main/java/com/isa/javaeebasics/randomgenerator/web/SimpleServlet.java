991ae8ea14951c5e891894e52242f2a31cf35680
strzyga <dominika.puzio@gmail.com>
2017-09-14 14:57:51 +0200

    Html

--- src/main/java/com/isa/javaeebasics/randomgenerator/web/SimpleServlet.java
+++ src/main/java/com/isa/javaeebasics/randomgenerator/web/SimpleServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/generator")
 public class SimpleServlet extends HttpServlet{
@@ -15,6 +16,16 @@ public class SimpleServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         NumbersGenerator numbers = new NumbersGenerator();
-        resp.getWriter().print(numbers.getRandomInt(100));
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html>");
+        writer.println("<body text=\"green\">");
+        writer.println("Random number: "+numbers.getRandomInt(100));
+        writer.println("</body>");
+        writer.println("</html>");
     }
 }
