2c4ee92a48694bd587306aa6381a6062917637aa
Łukasz Rybka <darkstariw@gmail.com>
2017-10-10 14:50:38 +0200

    JSP actions.

---
+++ messageboard/src/main/java/com/infoshareacademy/messageboard/model/CounterInfo.java
@@ -0,0 +1,24 @@
+package com.infoshareacademy.messageboard.model;
+
+import java.util.Date;
+
+public class CounterInfo {
+    private Date date;
+    private Integer count;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
--- messageboard/src/main/webapp/counter.jsp
+++ messageboard/src/main/webapp/counter.jsp
@@ -4,6 +4,18 @@
     <title>Counter</title>
 </head>
 <body>
+    <jsp:useBean id="date" scope="page" class="java.util.Date" />
     <h2>Count: ${counter}</h2>
+    <h3>Current date: ${date}</h3>
+
+    <jsp:useBean id="info" scope="page" class="com.infoshareacademy.messageboard.model.CounterInfo" />
+
+    <%
+        info.setCount(3);
+        info.setDate(date);
+    %>
+
+    <h4>${info.count} // ${info.date}</h4>
+
 </body>
 </html>
